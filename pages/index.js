a4e262705282ca411737b43ca03ac57d454482a6
TUSHAR KHARAT <kharat.tushar11@gmail.com>
2020-08-27 17:28:27 +0530

    Added br

--- pages/index.js
+++ pages/index.js
@@ -34,8 +34,8 @@ const Home = () => (
 					<main className="mx-auto max-w-screen-xl px-4 sm:px-6">
 						<div className="text-center h-screen flex items-center justify-center">
 							<h2 className="text-xl -mt-1/3 sm:-mt-28 font-comfortaa-bold grid tracking-tight leading-10 font-thin text-gray-200 sm:text-4xl md:leading-none lg:text-6xl">
-								<span className="text-lg font-comfortaa-bold tracking-wide text-gray-100 sm:text-2xl lg:text-base xl:text-4xl md:pb-5">Coming Soon</span>
-								News.Is.Dead.Long.Live.Video.News
+								<span className="text-lg font-comfortaa-bold tracking-wide text-gray-100 sm:text-2xl lg:text-base xl:text-4xl md:pb-5">COMING SOON</span>
+								News.Is.Dead<br/>Long.Live.Video.News
 							</h2>
 						</div>
 					</main>
